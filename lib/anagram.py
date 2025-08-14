0a79cca122f232c9e2affeef61e20ac3a3c92bdc
kurui <kuruijoshua@gmail.com>
2025-08-14 12:57:57 +0300

    Add Anagram class with match() method for detecting anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        result = []
+        for candidate in candidates:
+            if candidate.lower() != self.word.lower() and sorted(candidate.lower()) == sorted(self.word.lower()):
+                result.append(candidate)
+        return result
